93ec94273602fce511d61c1f7d74c6eeb3d0eba3
djdannel <dalvarezdevs@gmail.com>
2022-11-07 11:20:26 -0500

    Create Zircon items

--- src/main/java/net/djdannel/tutorialmod/TutorialMod.java
+++ src/main/java/net/djdannel/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.djdannel.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.djdannel.tutorialmod.item.ModItems;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -21,6 +22,7 @@ public class TutorialMod
     public TutorialMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.register(modEventBus);
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
     }
